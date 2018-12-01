09488ebacb783a3544863b6836142166543771ef
David Eddy <dneddy@comcast.net>
2018-11-30 16:23:53 -0800

    Created finish line gate to complete a stage

---
+++ Assets/Scripts/GoalTrigger.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class GoalTrigger : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        print("Goal reached");
+
+        //TODO: End current stage, revert to main menu
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        
+    }
+}
