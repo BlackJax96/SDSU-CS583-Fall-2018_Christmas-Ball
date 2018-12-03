a8849e798d525dd03c5730c4dd7844c5dc01a727
Unknown <travisdattilo@yahoo.com>
2018-12-02 19:05:24 -0800

    Added Menu Scene, Created basic scene management script (incomplete)

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+
+    public void ChooseScene(string levelName)
+    {
+        SceneManager.LoadScene(levelName);
+    }
+
+    public void QuitGame()
+    {
+        //Debug.Log("GAME QUIT");
+        Application.Quit();
+    }
+
+}
+
