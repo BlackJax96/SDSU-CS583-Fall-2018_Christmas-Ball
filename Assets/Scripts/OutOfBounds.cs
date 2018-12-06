86f8bf9827925ee816a772ae26ead07f3afe46ae
Unknown <travisdattilo@yahoo.com>
2018-12-05 21:29:06 -0800

    Added OutOfBounds script to restart level when ball falls to certain y

--- Assets/Scripts/OutOfBounds.cs
+++ Assets/Scripts/OutOfBounds.cs
@@ -1,16 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class OutOfBounds : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private GameObject ballObject;
+    private string currentScene;
+    public float yValueToRestart;
+   
+    void Start()
+    {
+        ballObject = GameObject.FindGameObjectWithTag("PlayerBall");
+        currentScene = SceneManager.GetActiveScene().name;
+    }
+
+    void Update()
+    {
+        if(ballObject.transform.position.y < yValueToRestart)
+        {
+            SceneManager.LoadScene(currentScene);
+        }
+    }
 }
