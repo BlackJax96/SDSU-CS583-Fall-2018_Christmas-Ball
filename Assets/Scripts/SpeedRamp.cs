106c57c8ba5b5776d3e38339a01f0b2f69c39bf2
Unknown <travisdattilo@yahoo.com>
2018-12-06 00:27:44 -0800

    Created a "SpeedRamp" prefab to launch ball forward

---
+++ Assets/Scripts/SpeedRamp.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedRamp : MonoBehaviour {
+
+    public float power;
+    public Rigidbody rigidBody;
+    private GameObject ball; 
+
+	void Start ()
+    {
+        gameObject.GetComponent<Renderer>().material.color = Color.green;
+        ball = GameObject.FindGameObjectWithTag("PlayerBall");
+        rigidBody = ball. GetComponent<Rigidbody>();
+    }
+	
+    void OnTriggerEnter(Collider impact)
+    {
+        if (impact.CompareTag("PlayerBall"))
+        {
+            print("ramp");
+            rigidBody.AddForce(transform.forward * power);
+        }
+    }
+ 
+
+}
