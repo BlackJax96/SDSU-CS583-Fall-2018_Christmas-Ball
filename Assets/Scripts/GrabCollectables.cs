406557d341059df7f74908d75970c2c562e15069
Unknown <travisdattilo@yahoo.com>
2018-12-09 03:36:31 -0800

    Present collectible now spins and levitates up and down (WIP)

--- Assets/Scripts/GrabCollectables.cs
+++ Assets/Scripts/GrabCollectables.cs
@@ -4,13 +4,28 @@ using UnityEngine;
 
 public class GrabCollectables : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+    private GameObject present;
+    private GameObject axis; 
+    public float spinSpeed;
+
+    public float amplitude = 0.5f;
+    public float frequency = 1f;
+    Vector3 tmp = new Vector3();
+    Vector3 postOffset = new Vector3();
+
+    
+    void Start () {
+        present = GameObject.FindGameObjectWithTag("PresentCollectable");
+        axis = GameObject.Find("CollectableAxis");
+        postOffset = transform.position;
 	}
 	
-	// Update is called once per frame
 	void Update () {
-		
-	}
+        //transform.Rotate(Vector3.up, speed * Time.deltaTime);
+        transform.RotateAround(axis.transform.position, Vector3.up, Time.deltaTime * spinSpeed); // rotate around a vertical axis
+        tmp = postOffset;
+        tmp.y += Mathf.Sin(Time.fixedTime * Mathf.PI * frequency) * amplitude;
+        transform.position = tmp;
+
+    }
 }
