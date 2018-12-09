8f495e13b9357211c9f545916e7f6e0abdf927f7
Unknown <travisdattilo@yahoo.com>
2018-12-09 14:44:50 -0800

    Created a present collectible prefab to place throughout each level

--- Assets/Scripts/GrabCollectables.cs
+++ Assets/Scripts/GrabCollectables.cs
@@ -3,29 +3,39 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GrabCollectables : MonoBehaviour {
+    
+    public GameObject present;
+    public GameObject axis;
+    public float spinSpeed = 50f;
 
-    private GameObject present;
-    private GameObject axis; 
-    public float spinSpeed;
-
-    public float amplitude = 0.5f;
-    public float frequency = 1f;
+    public float amplitude = 0.2f;
+    public float frequency = 0.4f;
     Vector3 tmp = new Vector3();
     Vector3 postOffset = new Vector3();
 
     
     void Start () {
-        present = GameObject.FindGameObjectWithTag("PresentCollectable");
-        axis = GameObject.Find("CollectableAxis");
         postOffset = transform.position;
 	}
 	
 	void Update () {
-        //transform.Rotate(Vector3.up, speed * Time.deltaTime);
-        transform.RotateAround(axis.transform.position, Vector3.up, Time.deltaTime * spinSpeed); // rotate around a vertical axis
+        if(present != null) // if the present hasn't been destroyed yet
+        {
+            present.transform.RotateAround(axis.transform.position, Vector3.up, Time.deltaTime * spinSpeed); // rotate around a vertical axis
+        }
+            
+        /*
         tmp = postOffset;
-        tmp.y += Mathf.Sin(Time.fixedTime * Mathf.PI * frequency) * amplitude;
+        tmp.y += Mathf.Sin(Time.fixedTime * Mathf.PI * frequency) * amplitude;  // levitating up and down
         transform.position = tmp;
-
+        */
+    }
+    
+    void OnTriggerEnter(Collider impact)
+    {
+        if (impact.gameObject.tag.Equals("PlayerBall"))
+        {
+            Destroy(present);
+        }
     }
 }
